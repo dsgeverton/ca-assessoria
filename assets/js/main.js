bd7a55b9de577e73520b7c237ccfd61dc5cf3d0c
Everton Gonçalves <everton.goncalves@ascbrazil.com.br>
2023-05-01 12:33:35 -0300

    ajustando layout e esquema de cores

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,18 +1,35 @@
 // seleciona o elemento div
 const logo = document.querySelector('#logo');
 const icon = document.querySelector('#icon');
+icon.style.display = 'none';
+
+icon.addEventListener('transitionend', () => {
+    const logoBox = document.querySelector('.logo');
+    console.log('oi')
+    if (logo.style.display === 'none') {
+        logoBox.classList.add('transition');
+    } else {
+        logoBox.classList.remove('transition');
+    }
+});
 
 // adiciona o evento scroll ao objeto window
-window.addEventListener('scroll', function() {
-  // obtém a posição vertical da janela de visualização
-  const scrollPosition = window.scrollY;
+window.addEventListener('scroll', function () {
+    // obtém a posição vertical da janela de visualização
+    const logoBox = document.querySelector('.logo');
+    const scrollPosition = window.scrollY;
+    // verifica se a posição vertical é maior que 100 pixels
+    if (scrollPosition >= 1) {
+        // altera a cor de fundo do elemento div
+        logoBox.classList.add('transition');
 
-  // verifica se a posição vertical é maior que 100 pixels
-  if (scrollPosition > 100) {
-    // altera a cor de fundo do elemento div
-    logo.style.display = 'none';
-  } else {
-    // se não, define a cor de fundo como transparente
-    logo.style.display = 'block';
-  }
+        logo.classList.add('hide')
+        
+        icon.style.display = 'block';
+    } else {
+        // se não, define a cor de fundo como transparente
+        logoBox.classList.remove('transition');
+        logo.classList.remove('hide')
+        icon.style.display = 'none';
+    }
 });
